aea137f806a56c619ac055ceed10c458b0ab545a
Woori <rose5307@naver.com>
2022-03-27 00:12:38 +0900

    make add and paint todo list item function

--- js/todo.js
+++ js/todo.js
@@ -1,4 +1,23 @@
 const messageInput = document.querySelector('.schedule-message-input');
 const todoInput = document.querySelector('.schedule-todo-input');
+const todoInputForm = document.querySelector('.schedule-todo-form');
+const todoList = document.querySelector('.schedule-todo-list');
 
+let todoArr = [];
 
+const paintTodoItem = (todoText) => {
+    const newListItem = document.createElement('li');
+    newListItem.textContent = todoText;
+    newListItem.classList.add = "list-item";
+    todoList.appendChild(newListItem);
+}
+
+const addTodoItem = (event) => {
+    event.preventDefault();
+    const newTodo = event.target[0].value;
+    todoArr.push(newTodo);
+    paintTodoItem(newTodo);
+    todoInput.value = '';
+}
+
+todoInputForm.addEventListener('submit', addTodoItem);
