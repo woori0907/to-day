a563ce43a31d125e594e25eed5a04f660c11969c
Woori <rose5307@naver.com>
2022-03-27 22:11:21 +0900

    make delete, check todo item function

--- js/todo.js
+++ js/todo.js
@@ -1,15 +1,25 @@
 const messageInput = document.querySelector('.schedule-message-input');
 const todoInput = document.querySelector('.schedule-todo-input');
-const todoInputForm = document.querySelector('.schedule-todo-form');
-const todoList = document.querySelector('.schedule-todo-list');
+const todoInputForm = document.querySelector('.todo-form');
+const todoList = document.querySelector('.todo-list');
 
 let todoArr = [];
 
 const paintTodoItem = (todoText) => {
     const newListItem = document.createElement('li');
-    newListItem.textContent = todoText;
-    newListItem.classList.add = "list-item";
+    const itemText = document.createElement('span');
+    const checkBox = document.createElement('i');
+    const deleteBtn = document.createElement('i');
+    itemText.innerText = todoText;
+    deleteBtn.classList.add("fa-solid", "fa-xmark","list-deletebtn");
+    checkBox.classList.add ("fa-regular", "fa-square","list-checkbox");
+    newListItem.classList.add("list-item");
+    newListItem.appendChild(checkBox);
+    newListItem.appendChild(itemText);
+    newListItem.appendChild(deleteBtn);
     todoList.appendChild(newListItem);
+    deleteBtn.addEventListener("click", deleteTotoItem);
+    checkBox.addEventListener("click", checkTodoItem);
 }
 
 const addTodoItem = (event) => {
@@ -18,6 +28,22 @@ const addTodoItem = (event) => {
     todoArr.push(newTodo);
     paintTodoItem(newTodo);
     todoInput.value = '';
+}
+
+const checkTodoItem = (event) => {
+    const item = event.target.parentNode;
+    if(item.classList.contains('is-checked')){
+        item.classList.remove('is-checked');
+    }
+    else{
+        item.classList.add('is-checked');
+    }
+}
+
+const deleteTotoItem = (event) =>{
+    const newTodo = todoArr.filter((item) => item == event.target.parentNode.textContent);
+    todoArr = newTodo;
+    event.target.parentNode.remove();
 }
 
 todoInputForm.addEventListener('submit', addTodoItem);
